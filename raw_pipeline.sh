03dc394d476b2f21f332acfb64dcff09b00ad2e2
Pg-Pi3Gey <pi3gey@gmail.com>
2023-02-24 23:13:40 +0530

    :sparkles: add multiqc report for untrimmed sequence

--- raw_pipeline.sh
+++ raw_pipeline.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-##  start the timer
+###  start the timer
 SECONDS=0
 
 # change working directory
 cd ~/CHIKV_DEG
 
-# STEP 1: Run fastqc #
+## STEP 1: Run fastqc ##
 mkdir -p ~/CHIKV_DEG/results/fastqc/untrimmed
-cd ~/CHIKV_DEG/raw_seq/untrimmed_seq
-mkdir -p ~/CHIKV_DEG/results/fastqc/untrimmed
-fastqc -t 16 *.fastq.gz -o ~/CHIKV_DEG/results/fastqc/untrimmed
+fastqc -t 16 ~/CHIKV_DEG/raw_seq/untrimmed_seq/*.fastq.gz -o ~/CHIKV_DEG/results/fastqc/untrimmed
 echo "|> Quality Check via FASTQC on untrimmed sequencence completed <|"
-# create single report via multiqc
+# create single report via multiqc #
 multiqc ~/CHIKV_DEG/results/fastqc/untrimmed -o ~/CHIKV_DEG/results/multiqc -n untrimmed_multiqc_report.html
 
 ##  Total runtime
