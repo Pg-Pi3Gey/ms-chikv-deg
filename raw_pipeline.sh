6af8738dad4fc23b1aaf7899ad803dd3c565f185
Pg-Pi3Gey <pi3gey@gmail.com>
2023-02-24 13:36:33 +0530

    Step1: FASTQC

--- raw_pipeline.sh
+++ raw_pipeline.sh
@@ -5,6 +5,13 @@ SECONDS=0
 # change working directory
 cd ~/CHIKV_DEG
 
+# STEP 1: Run fastqc #
+mkdir -p ~/CHIKV_DEG/results/fastqc/untrimmed
+cd ~/CHIKV_DEG/raw_seq/untrimmed_seq
+mkdir -p ~/CHIKV_DEG/results/fastqc/untrimmed
+fastqc -t 16 *.fastq.gz -o ~/CHIKV_DEG/results/fastqc/untrimmed
+echo "|> Quality Check via FASTQC on untrimmed sequencence completed <|"
+
 ##  Total runtime
 duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
