c3e95b0e21848662c08f81c3ddfdaa92834d9617
Pi3Gey <pi3gey@gmail.com>
2023-02-24 10:57:33 +0530

    initial dataset download

---
+++ download_fq.sh
@@ -0,0 +1,16 @@
+﻿#!/usr/bin/env bash
+
+#   PHASE0  #
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR852/004/SRR8521514/SRR8521514_1.fastq.gz -o SRR8521514_infected_PZIKAG22_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR852/004/SRR8521514/SRR8521514_2.fastq.gz -o SRR8521514_infected_PZIKAG22_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR852/007/SRR8521487/SRR8521487_1.fastq.gz -o SRR8521487_infected_PZIAKG11_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR852/007/SRR8521487/SRR8521487_2.fastq.gz -o SRR8521487_infected_PZIAKG11_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR852/006/SRR8521486/SRR8521486_1.fastq.gz -o SRR8521486_infected_PZIKAG12_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR852/006/SRR8521486/SRR8521486_2.fastq.gz -o SRR8521486_infected_PZIKAG12_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR852/004/SRR8521494/SRR8521494_1.fastq.gz -o SRR8521494_infected_PZIKAG15_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR852/004/SRR8521494/SRR8521494_2.fastq.gz -o SRR8521494_infected_PZIKAG15_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR852/006/SRR8521516/SRR8521516_1.fastq.gz -o SRR8521516_infected_PZIKAG14_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR852/006/SRR8521516/SRR8521516_2.fastq.gz -o SRR8521516_infected_PZIKAG14_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR852/009/SRR8521489/SRR8521489_1.fastq.gz -o SRR8521489_infected_PZIKAG09_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR852/009/SRR8521489/SRR8521489_2.fastq.gz -o SRR8521489_infected_PZIKAG09_2.fastq.gz
