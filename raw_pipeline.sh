94490e63def2ef16fd14afb11cab41953382a1c3
Pg-Pi3Gey <pi3gey@gmail.com>
2023-02-25 17:29:38 +0530

    Step2: trimming

--- raw_pipeline.sh
+++ raw_pipeline.sh
@@ -5,12 +5,36 @@ SECONDS=0
 # change working directory
 cd ~/CHIKV_DEG
 
-## STEP 1: Run fastqc ##
+## STEP 1: Qualitycheck via FASTQC ##
 mkdir -p ~/CHIKV_DEG/results/fastqc/untrimmed
 fastqc -t 16 ~/CHIKV_DEG/raw_seq/untrimmed_seq/*.fastq.gz -o ~/CHIKV_DEG/results/fastqc/untrimmed
 echo "|> Quality Check via FASTQC on untrimmed sequencence completed <|"
 # create single report via multiqc #
 multiqc ~/CHIKV_DEG/results/fastqc/untrimmed -o ~/CHIKV_DEG/results/multiqc -n untrimmed_multiqc_report.html
+
+
+## STEP 2: Trimming bad reads via Trimmomatic ##
+cd ~/CHIKV_DEG/raw_seq/untrimmed_seq/
+cp ~/CHIKV_DEG/tools/Trimmomatic-0.39/adapters/TruSeq3-PE.fa .
+for infile in *_1.fastq.gz
+do
+    base=$(basename ${infile} _1.fastq.gz)
+    java -jar ~/CHIKV_DEG/tools/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 16 -phred33 ${infile} ${base}_2.fastq.gz \
+    ${base}_1_paired.fastq.gz ${base}_1_unpaired.fastq.gz \
+    ${base}_2_paired.fastq.gz ${base}_2_unpaired.fastq.gz \
+    ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:True LEADING:5 TRAILING:5 SLIDINGWINDOW:5:20 MINLEN:35
+done
+# move trimmed sequence to different folder #
+mkdir ../trimmed_seq
+mv *pair* ../trimmed_seq
+echo "|> Trimmomatic finished running! <|"
+
+# Quality check the trimmed sequences via FASTQC #
+mkdir -p ~/CHIKV_DEG/results/fastqc/trimmed
+fastqc -t 16 ~/CHIKV_DEG/raw_seq/trimmed_seq/*.fastq.gz -o ~/CHIKV_DEG/results/fastqc/trimmed
+# create single report via multiqc #
+multiqc ~/CHIKV_DEG/results/fastqc/trimmed -o ~/CHIKV_DEG/results/multiqc -n trimmed_multiqc_report.html
+
 
 ##  Total runtime
 duration=$SECONDS
